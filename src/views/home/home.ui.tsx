aab2b3150c8736a7b184cc968f5b6ab1cbc3363d
Corbin Crutchley <git@crutchcorn.dev>
2025-12-16 23:16:25 -0800

    feat: add to UI

--- src/views/home/home.ui.tsx
+++ src/views/home/home.ui.tsx
@@ -1,6 +1,24 @@
-import { Text } from 'react-native';
-import { styles } from './home.styles';
+import { useRef } from 'react';
+import {InkCanvas, InkCanvasRef} from '../../components/InkCanvas';
+import { Button, View } from 'react-native';
 
 export function HomeUI() {
-  return <Text style={styles.container}>Home Screen</Text>;
+  const canvasRef = useRef<InkCanvasRef>(null);
+
+  const handleClear = () => {
+    canvasRef.current?.clear();
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <InkCanvas
+        ref={canvasRef}
+        brushColor="#0000FF"
+        brushSize={8}
+        brushFamily="pen"
+        style={{flex: 1}}
+      />
+      <Button title="Clear" onPress={handleClear} />
+    </View>
+  );
 }
