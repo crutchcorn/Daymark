2d4ec570a753caa58bc2a8eecefbbba32b57412f
Corbin Crutchley <git@crutchcorn.dev>
2025-12-23 01:50:49 -0800

    chore: run Nx init

--- apps/android-prototype/build.gradle.kts
+++ apps/android-prototype/build.gradle.kts
@@ -1,6 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("dev.nx.gradle.project-graph") version("0.1.10")
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+}
+allprojects {
+    apply {
+        plugin("dev.nx.gradle.project-graph")
+    }
 }
