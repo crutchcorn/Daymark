d3c60c0a1d035a24d7d7cac103f273bc93df48f4
Corbin Crutchley <git@crutchcorn.dev>
2025-12-23 00:38:38 -0800

    chore: port existing inkcanvas and editor views

--- android-prototype/app/src/main/java/app/drawmark/android/prototype/MainActivity.kt
+++ android-prototype/app/src/main/java/app/drawmark/android/prototype/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import app.drawmark.android.prototype.ink.InkEditor
 import app.drawmark.android.prototype.ui.theme.DrawmarkAndroidPrototypeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +19,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             DrawmarkAndroidPrototypeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    InkEditor(
+                        context = LocalContext.current,
+                        modifier = Modifier.padding(innerPadding).fillMaxSize()
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DrawmarkAndroidPrototypeTheme {
-        Greeting("Android")
     }
 }
